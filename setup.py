738496c525a6fce5ae4286858fced076c70ec465
Jeremy Singer-Vine <jsvine@gmail.com>
2016-03-07 16:53:39 -0500

    ASCII-ify setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ base_reqs = [
 
 setup(
     name="pdfplumber",
-    description="Plumb a PDF for detailed information about each char, rectangle, and line — and easily extract text and tables.",
+    description="Plumb a PDF for detailed information about each char, rectangle, and line -- and easily extract text and tables.",
     version=version,
     packages=find_packages(exclude=["test",]),
     tests_require=[ "nose", "pandas" ] + base_reqs,
