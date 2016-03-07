b8b384e186c7a743218173de1cf3a3372cbb904c
Jeremy Singer-Vine <jsvine@gmail.com>
2016-03-06 20:24:13 -0500

    Bump to v0.3.1

--- pdfplumber/__init__.py
+++ pdfplumber/__init__.py
@@ -5,7 +5,7 @@ import pdfminer.pdftypes
 pdfminer.pdftypes.STRICT = False
 pdfminer.pdfinterp.STRICT = False
 
-VERSION_TUPLE = (0, 3, 0)
+VERSION_TUPLE = (0, 3, 1)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 def load(file_or_buffer, **kwargs):
