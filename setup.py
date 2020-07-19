48eaa515577a4fc6397afbd50851b74d45297097
Jeremy Singer-Vine <jsvine@gmail.com>
2020-07-19 10:35:09 -0400

    Move from Pipfile back to standard pip req files

--- setup.py
+++ setup.py
@@ -4,26 +4,24 @@ import subprocess
 
 NAME = "pdfplumber"
 HERE = os.path.abspath(os.path.dirname(__file__))
+
 version_ns = {}
 with open(os.path.join(HERE, NAME, '_version.py')) as f:
     exec(f.read(), {}, version_ns)
 
-base_reqs = [
-    "chardet",
-    "pycryptodome",
-    "unicodecsv>=0.14.1",
-    "pdfminer.six==20200104",
-    "pillow>=7.0.0",
-    "wand"
-]
+with open(os.path.join(HERE, "requirements.txt")) as f:
+    base_reqs = f.read().strip().split("\n")
+
+with open(os.path.join(HERE, "requirements-dev.txt")) as f:
+    dev_reqs = f.read().strip().split("\n")
 
 setup(
     name=NAME,
     description="Plumb a PDF for detailed information about each char, rectangle, and line.",
     version=version_ns['__version__'],
     packages=find_packages(exclude=["test",]),
-    tests_require=[ "nose", "pandas" ] + base_reqs,
-    install_requires=base_reqs,
+    tests_require = base_reqs + dev_reqs,
+    install_requires = base_reqs,
     entry_points={
         "console_scripts": [ "pdfplumber = pdfplumber.cli:main" ] 
     }
