jsvine/pdfplumber

commit 9587cc7d2292a1eae7a0150ab406f9365944266f

Add type annotations and refactor accordingly
A fairly large commit, adding type annotations/hints to the entire
library, and refactoring the library accordingly.

Most of the refactoring changes should have no practical effect on
usage, but several others are notable:

- Added `TableSettings` class, a behind-the-scenes handler for managing
  and validating table-extraction settings.
- Renamed the positional argument to `.to_csv(...)` and `.to_json(...)`
  from `types` to `object_types`.
- Tweaked the output of `.to_json(...)` so that, if an object type is
  not present for a given page, it has no key in the page's object
  representation.
- Removed `utils.filter_objects(...)` and move the functionality to
  within the `FilteredPage.objects` property calculation, the only part
  of the library that used it.
- Removed code that sets `pdfminer.pdftypes.STRICT = True` and
  `pdfminer.pdfinterp.STRICT = True`, since that [has now been the
  default for a
  while](https://github.com/pdfminer/pdfminer.six/commit/9439a3a31a347836aad1c1226168156125d9505f).

pdfplumber/page.py
@@ -1,11 +1,24 @@
 import re
+from typing import TYPE_CHECKING, Any, Callable, Dict, Generator, List, Optional, Tuple
 
 from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import (
+    LTChar,
+    LTComponent,
+    LTContainer,
+    LTCurve,
+    LTItem,
+    LTLine,
+    LTPage,
+    LTRect,
+)
 from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
 
 from . import utils
+from ._typing import T_bbox, T_num, T_obj, T_obj_list
 from .container import Container
-from .table import TableFinder
+from .table import T_table_settings, Table, TableFinder, TableSettings
 from .utils import resolve_all
 
 lt_pat = re.compile(r"^LT")
@@ -42,12 +55,25 @@ ALL_ATTRS = set(
 )
 
 
-class Page(Container):
-    cached_properties = Container.cached_properties + ["_layout"]
-    is_original = True
+if TYPE_CHECKING:  # pragma: nocover
+    from .display import PageImage
+    from .pdf import PDF
 
-    def __init__(self, pdf, page_obj, page_number=None, initial_doctop=0):
+
+class Page(Container):
+    cached_properties: List[str] = Container.cached_properties + ["_layout"]
+    is_original: bool = True
+    pages = None
+
+    def __init__(
+        self,
+        pdf: "PDF",
+        page_obj: PDFPage,
+        page_number: int,
+        initial_doctop: T_num = 0,
+    ):
         self.pdf = pdf
+        self.root_page = self
         self.page_obj = page_obj
         self.page_number = page_number
         _rotation = resolve_all(self.page_obj.attrs.get("Rotate", 0))
@@ -62,31 +88,32 @@ class Page(Container):
         self.mediabox = resolve_all(mediabox) or self.cropbox
         m = self.mediabox
 
-        if self.rotation in [90, 270]:
-            self.bbox = (
+        self.bbox: T_bbox = (
+            (
                 min(m[1], m[3]),
                 min(m[0], m[2]),
                 max(m[1], m[3]),
                 max(m[0], m[2]),
             )
-        else:
-            self.bbox = (
+            if self.rotation in [90, 270]
+            else (
                 min(m[0], m[2]),
                 min(m[1], m[3]),
                 max(m[0], m[2]),
                 max(m[1], m[3]),
             )
+        )
 
     @property
-    def width(self):
+    def width(self) -> T_num:
         return self.bbox[2] - self.bbox[0]
 
     @property
-    def height(self):
+    def height(self) -> T_num:
         return self.bbox[3] - self.bbox[1]
 
     @property
-    def layout(self):
+    def layout(self) -> LTPage:
         if hasattr(self, "_layout"):
             return self._layout
         device = PDFPageAggregator(
@@ -96,12 +123,12 @@ class Page(Container):
         )
         interpreter = PDFPageInterpreter(self.pdf.rsrcmgr, device)
         interpreter.process_page(self.page_obj)
-        self._layout = device.get_result()
+        self._layout: LTPage = device.get_result()
         return self._layout
 
     @property
-    def annots(self):
-        def parse(annot):
+    def annots(self) -> T_obj_list:
+        def parse(annot: T_obj) -> T_obj:
             rect = annot["Rect"]
 
             a = annot.get("A", {})
@@ -142,20 +169,20 @@ class Page(Container):
         return list(map(parse, raw))
 
     @property
-    def hyperlinks(self):
+    def hyperlinks(self) -> T_obj_list:
         return [a for a in self.annots if a["uri"] is not None]
 
     @property
-    def objects(self):
+    def objects(self) -> Dict[str, T_obj_list]:
         if hasattr(self, "_objects"):
             return self._objects
-        self._objects = self.parse_objects()
+        self._objects: Dict[str, T_obj_list] = self.parse_objects()
         return self._objects
 
-    def process_object(self, obj):
+    def process_object(self, obj: LTItem) -> T_obj:
         kind = re.sub(lt_pat, "", obj.__class__.__name__).lower()
 
-        def process_attr(item):
+        def process_attr(item: Tuple[str, Any]) -> Optional[Tuple[str, Any]]:
             k, v = item
             if k in ALL_ATTRS:
                 res = resolve_all(v)
@@ -168,17 +195,15 @@ class Page(Container):
         attr["object_type"] = kind
         attr["page_number"] = self.page_number
 
-        if hasattr(obj, "graphicstate"):
+        if isinstance(obj, LTChar):
+            attr["text"] = obj.get_text()
             gs = obj.graphicstate
             attr["stroking_color"] = gs.scolor
             attr["non_stroking_color"] = gs.ncolor
 
-        if hasattr(obj, "get_text"):
-            attr["text"] = obj.get_text()
+        if isinstance(obj, LTCurve) and not isinstance(obj, (LTRect, LTLine)):
 
-        if kind == "curve":
-
-            def point2coord(pt):
+            def point2coord(pt: Tuple[T_num, T_num]) -> Tuple[T_num, T_num]:
                 x, y = pt
                 return (x, self.height - y)
 
@@ -191,10 +216,12 @@ class Page(Container):
 
         return attr
 
-    def iter_layout_objects(self, layout_objects):
+    def iter_layout_objects(
+        self, layout_objects: List[LTComponent]
+    ) -> Generator[T_obj, None, None]:
         for obj in layout_objects:
             # If object is, like LTFigure, a higher-level object ...
-            if hasattr(obj, "_objs"):
+            if isinstance(obj, LTContainer):
                 # and LAParams is passed, process the object itself.
                 if self.pdf.laparams is not None:
                     yield self.process_object(obj)
@@ -203,8 +230,8 @@ class Page(Container):
             else:
                 yield self.process_object(obj)
 
-    def parse_objects(self):
-        objects = {}
+    def parse_objects(self) -> Dict[str, T_obj_list]:
+        objects: Dict[str, T_obj_list] = {}
         for obj in self.iter_layout_objects(self.layout._objs):
             kind = obj["object_type"]
             if kind in ["anno"]:
@@ -214,76 +241,82 @@ class Page(Container):
             objects[kind].append(obj)
         return objects
 
-    def debug_tablefinder(self, table_settings={}):
-        return TableFinder(self, table_settings)
+    def debug_tablefinder(
+        self, table_settings: Optional[T_table_settings] = None
+    ) -> TableFinder:
+        tset = TableSettings.resolve(table_settings)
+        return TableFinder(self, tset)
 
-    def find_tables(self, table_settings={}):
-        return TableFinder(self, table_settings).tables
+    def find_tables(
+        self, table_settings: Optional[T_table_settings] = None
+    ) -> List[Table]:
+        tset = TableSettings.resolve(table_settings)
+        return TableFinder(self, tset).tables
 
-    def extract_tables(self, table_settings={}):
-        table_settings = TableFinder.resolve_table_settings(table_settings)
-        tables = self.find_tables(table_settings)
+    def extract_tables(
+        self, table_settings: Optional[T_table_settings] = None
+    ) -> List[List[List[Optional[str]]]]:
+        tset = TableSettings.resolve(table_settings)
+        tables = self.find_tables(tset)
 
         extract_kwargs = {
-            k: table_settings["text_" + k]
-            for k in ["x_tolerance", "y_tolerance"]
-            if "text_" + k in table_settings
+            k: getattr(tset, "text_" + k) for k in ["x_tolerance", "y_tolerance"]
         }
 
         return [table.extract(**extract_kwargs) for table in tables]
 
-    def extract_table(self, table_settings={}):
-        table_settings = TableFinder.resolve_table_settings(table_settings)
-        tables = self.find_tables(table_settings)
+    def extract_table(
+        self, table_settings: Optional[T_table_settings] = None
+    ) -> Optional[List[List[Optional[str]]]]:
+        tset = TableSettings.resolve(table_settings)
+        tables = self.find_tables(tset)
 
         if len(tables) == 0:
             return None
 
         # Return the largest table, as measured by number of cells.
-        def sorter(x):
+        def sorter(x: Table) -> Tuple[int, T_num, T_num]:
             return (-len(x.cells), x.bbox[1], x.bbox[0])
 
         largest = list(sorted(tables, key=sorter))[0]
 
-        extract_kwargs = dict(
-            (k, table_settings["text_" + k])
-            for k in ["x_tolerance", "y_tolerance"]
-            if "text_" + k in table_settings
-        )
+        extract_kwargs = {
+            k: getattr(tset, "text_" + k) for k in ["x_tolerance", "y_tolerance"]
+        }
 
         return largest.extract(**extract_kwargs)
 
-    def extract_text(self, **kwargs):
+    def extract_text(self, **kwargs: Any) -> str:
         return utils.extract_text(
             self.chars, x_shift=self.bbox[0], y_shift=self.bbox[1], **kwargs
         )
 
-    def extract_words(self, **kwargs):
+    def extract_words(self, **kwargs: Any) -> T_obj_list:
         return utils.extract_words(self.chars, **kwargs)
 
-    def crop(self, bbox, relative=False):
+    def crop(self, bbox: T_bbox, relative: bool = False) -> "CroppedPage":
         return CroppedPage(self, bbox, relative=relative)
 
-    def within_bbox(self, bbox, relative=False):
+    def within_bbox(self, bbox: T_bbox, relative: bool = False) -> "CroppedPage":
         """
         Same as .crop, except only includes objects fully within the bbox
         """
         return CroppedPage(self, bbox, relative=relative, crop_fn=utils.within_bbox)
 
-    def filter(self, test_function):
+    def filter(self, test_function: Callable[[T_obj], bool]) -> "FilteredPage":
         return FilteredPage(self, test_function)
 
-    def dedupe_chars(self, **kwargs):
+    def dedupe_chars(self, **kwargs: Any) -> "FilteredPage":
         """
         Removes duplicate chars — those sharing the same text, fontname, size,
         and positioning (within `tolerance`) as other characters on the page.
         """
-        p = FilteredPage(self, True)
+        p = FilteredPage(self, lambda x: True)
         p._objects = {kind: objs for kind, objs in self.objects.items()}
         p._objects["char"] = utils.dedupe_chars(self.chars, **kwargs)
         return p
 
-    def to_image(self, **conversion_kwargs):
+    def to_image(self, **conversion_kwargs: Any) -> "PageImage":
         """
         For conversion_kwargs, see:
         http://docs.wand-py.org/en/latest/wand/image.html#wand.image.Image
@@ -295,27 +328,42 @@ class Page(Container):
             kwargs["resolution"] = DEFAULT_RESOLUTION
         return PageImage(self, **kwargs)
 
-    def __repr__(self):
+    def to_dict(self, object_types: Optional[List[str]] = None) -> Dict[str, Any]:
+        if object_types is None:
+            _object_types = list(self.objects.keys()) + ["annot"]
+        else:
+            _object_types = object_types
+        d = {
+            "page_number": self.page_number,
+            "initial_doctop": self.initial_doctop,
+            "rotation": self.rotation,
+            "cropbox": self.cropbox,
+            "mediabox": self.mediabox,
+            "bbox": self.bbox,
+            "width": self.width,
+            "height": self.height,
+        }
+        for t in _object_types:
+            d[t + "s"] = getattr(self, t + "s")
+        return d
+
+    def __repr__(self) -> str:
         return f"<Page:{self.page_number}>"
 
 
 class DerivedPage(Page):
-    is_original = False
+    is_original: bool = False
 
-    def __init__(self, parent_page):
+    def __init__(self, parent_page: Page):
         self.parent_page = parent_page
+        self.root_page = parent_page.root_page
         self.pdf = parent_page.pdf
         self.page_obj = parent_page.page_obj
         self.page_number = parent_page.page_number
         self.flush_cache(Container.cached_properties)
 
-        if type(parent_page) == Page:
-            self.root_page = parent_page
-        else:
-            self.root_page = parent_page.root_page
 
-
-def test_proposed_bbox(bbox, parent_bbox):
+def test_proposed_bbox(bbox: T_bbox, parent_bbox: T_bbox) -> None:
     bbox_area = utils.calculate_area(bbox)
     if bbox_area == 0:
         raise ValueError(f"Bounding box {bbox} has an area of zero.")
@@ -336,7 +384,13 @@ def test_proposed_bbox(bbox, parent_bbox):
 
 
 class CroppedPage(DerivedPage):
-    def __init__(self, parent_page, bbox, crop_fn=utils.crop_to_bbox, relative=False):
+    def __init__(
+        self,
+        parent_page: Page,
+        bbox: T_bbox,
+        crop_fn: Callable[[T_obj_list, T_bbox], T_obj_list] = utils.crop_to_bbox,
+        relative: bool = False,
+    ):
         if relative:
             o_x0, o_top, _, _ = parent_page.bbox
             x0, top, x1, bottom = bbox
@@ -349,22 +403,27 @@ class CroppedPage(DerivedPage):
         super().__init__(parent_page)
 
     @property
-    def objects(self):
+    def objects(self) -> Dict[str, T_obj_list]:
         if hasattr(self, "_objects"):
             return self._objects
-        self._objects = self.crop_fn(self.parent_page.objects, self.bbox)
+        self._objects: Dict[str, T_obj_list] = {
+            k: self.crop_fn(v, self.bbox) for k, v in self.parent_page.objects.items()
+        }
         return self._objects
 
 
 class FilteredPage(DerivedPage):
-    def __init__(self, parent_page, filter_fn):
+    def __init__(self, parent_page: Page, filter_fn: Callable[[T_obj], bool]):
         self.bbox = parent_page.bbox
         self.filter_fn = filter_fn
         super().__init__(parent_page)
 
     @property
-    def objects(self):
+    def objects(self) -> Dict[str, T_obj_list]:
         if hasattr(self, "_objects"):
             return self._objects
-        self._objects = utils.filter_objects(self.parent_page.objects, self.filter_fn)
+        self._objects: Dict[str, T_obj_list] = {
+            k: list(filter(self.filter_fn, v))
+            for k, v in self.parent_page.objects.items()
+        }
         return self._objects

pdfplumber/__init__.py
@@ -13,14 +13,4 @@ from . import utils
 from ._version import __version__
 from .pdf import PDF
 
-pdfminer.pdftypes.STRICT = False
-pdfminer.pdfinterp.STRICT = False
-
 open = PDF.open
-
-
-def set_debug(debug=0):
-    pdfminer.debug = debug
-
-
-set_debug(0)

pdfplumber/_typing.py
@@ -1,0 +1,9 @@
+from typing import Any, Dict, Iterable, List, Sequence, Tuple, Union
+
+T_seq = Sequence
+T_num = Union[int, float]
+T_point = Tuple[T_num, T_num]
+T_bbox = Tuple[T_num, T_num, T_num, T_num]
+T_obj = Dict[str, Any]
+T_obj_list = List[T_obj]
+T_obj_iter = Iterable[T_obj]

pdfplumber/cli.py
@@ -3,20 +3,20 @@ import argparse
 import json
 import sys
 from itertools import chain
+from typing import List
 
-from . import convert
 from .pdf import PDF
 
 
-def parse_page_spec(p_str):
+def parse_page_spec(p_str: str) -> List[int]:
     if "-" in p_str:
         start, end = map(int, p_str.split("-"))
-        return range(start, end + 1)
+        return list(range(start, end + 1))
     else:
         return [int(p_str)]
 
 
-def parse_args(args_raw):
+def parse_args(args_raw: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser("pdfplumber")
 
     parser.add_argument(
@@ -43,16 +43,16 @@ def parse_args(args_raw):
     return args
 
 
-def main(args_raw=sys.argv[1:]):
+def main(args_raw: List[str] = sys.argv[1:]) -> None:
     args = parse_args(args_raw)
-    converter = {"csv": convert.to_csv, "json": convert.to_json}[args.format]
-    kwargs = {
-        "csv": {"precision": args.precision},
-        "json": {"precision": args.precision, "indent": args.indent},
-    }[args.format]
 
     with PDF.open(args.infile, pages=args.pages, laparams=args.laparams) as pdf:
-        converter(pdf, sys.stdout, args.types, **kwargs)
+        if args.format == "csv":
+            pdf.to_csv(sys.stdout, args.types, precision=args.precision)
+        else:
+            pdf.to_json(
+                sys.stdout, args.types, precision=args.precision, indent=args.indent
+            )
 
 
 if __name__ == "__main__":

pdfplumber/container.py
@@ -1,99 +1,174 @@
+import csv
+import json
+from io import StringIO
 from itertools import chain
+from typing import Any, Dict, List, Optional, Set, TextIO
 
-from . import convert, utils
+from . import utils
+from ._typing import T_obj, T_obj_list
+from .convert import Serializer
+
+CSV_COLS_TO_PREPEND = [
+    "object_type",
+    "page_number",
+    "x0",
+    "x1",
+    "y0",
+    "y1",
+    "doctop",
+    "top",
+    "bottom",
+    "width",
+    "height",
+]
 
 
 class Container(object):
     cached_properties = ["_rect_edges", "_edges", "_objects"]
 
-    def flush_cache(self, properties=None):
+    @property
+    def pages(self) -> Optional[List[Any]]:
+        ...  # pragma: nocover
+
+    @property
+    def objects(self) -> Dict[str, T_obj_list]:
+        ...  # pragma: nocover
+
+    def to_dict(self, object_types: Optional[List[str]] = None) -> Dict[str, Any]:
+        ...  # pragma: nocover
+
+    def flush_cache(self, properties: Optional[List[str]] = None) -> None:
         props = self.cached_properties if properties is None else properties
         for p in props:
             if hasattr(self, p):
                 delattr(self, p)
 
-    def close_file(self):
-        """
-        A placeholder, to be overridden when necessary (as in PDF.open)
-        """
-        pass
-
-    def close(self):
-        self.flush_cache()
-        self.close_file()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
-
     @property
-    def rects(self):
+    def rects(self) -> T_obj_list:
         return self.objects.get("rect", [])
 
     @property
-    def lines(self):
+    def lines(self) -> T_obj_list:
         return self.objects.get("line", [])
 
     @property
-    def curves(self):
+    def curves(self) -> T_obj_list:
         return self.objects.get("curve", [])
 
     @property
-    def images(self):
+    def images(self) -> T_obj_list:
         return self.objects.get("image", [])
 
     @property
-    def chars(self):
+    def chars(self) -> T_obj_list:
         return self.objects.get("char", [])
 
     @property
-    def textboxverticals(self):
+    def textboxverticals(self) -> T_obj_list:
         return self.objects.get("textboxvertical", [])
 
     @property
-    def textboxhorizontals(self):
+    def textboxhorizontals(self) -> T_obj_list:
         return self.objects.get("textboxhorizontal", [])
 
     @property
-    def textlineverticals(self):
+    def textlineverticals(self) -> T_obj_list:
         return self.objects.get("textlinevertical", [])
 
     @property
-    def textlinehorizontals(self):
+    def textlinehorizontals(self) -> T_obj_list:
         return self.objects.get("textlinehorizontal", [])
 
     @property
-    def rect_edges(self):
+    def rect_edges(self) -> T_obj_list:
         if hasattr(self, "_rect_edges"):
             return self._rect_edges
         rect_edges_gen = (utils.rect_to_edges(r) for r in self.rects)
-        self._rect_edges = list(chain(*rect_edges_gen))
+        self._rect_edges: T_obj_list = list(chain(*rect_edges_gen))
         return self._rect_edges
 
     @property
-    def edges(self):
+    def edges(self) -> T_obj_list:
         if hasattr(self, "_edges"):
             return self._edges
         line_edges = list(map(utils.line_to_edge, self.lines))
-        self._edges = self.rect_edges + line_edges
+        self._edges: T_obj_list = self.rect_edges + line_edges
         return self._edges
 
     @property
-    def horizontal_edges(self):
-        def test(x):
-            return x["orientation"] == "h"
+    def horizontal_edges(self) -> T_obj_list:
+        def test(x: T_obj) -> bool:
+            return bool(x["orientation"] == "h")
 
         return list(filter(test, self.edges))
 
     @property
-    def vertical_edges(self):
-        def test(x):
-            return x["orientation"] == "v"
+    def vertical_edges(self) -> T_obj_list:
+        def test(x: T_obj) -> bool:
+            return bool(x["orientation"] == "v")
 
         return list(filter(test, self.edges))
 
+    def to_json(
+        self,
+        stream: Optional[TextIO] = None,
+        object_types: Optional[List[str]] = None,
+        precision: Optional[int] = None,
+        indent: Optional[int] = None,
+    ) -> Optional[str]:
 
-Container.to_json = convert.to_json
-Container.to_csv = convert.to_csv
+        data = self.to_dict(object_types)
+
+        serialized = Serializer(precision=precision).serialize(data)
+
+        if stream is None:
+            return json.dumps(serialized, indent=indent)
+        else:
+            json.dump(serialized, stream, indent=indent)
+            return None
+
+    def to_csv(
+        self,
+        stream: Optional[TextIO] = None,
+        object_types: Optional[List[str]] = None,
+        precision: Optional[int] = None,
+    ) -> Optional[str]:
+        if stream is None:
+            stream = StringIO()
+            to_string = True
+        else:
+            to_string = False
+
+        if object_types is None:
+            object_types = list(self.objects.keys()) + ["annot"]
+
+        objs = []
+        fields: Set[str] = set()
+
+        pages = [self] if self.pages is None else self.pages
+
+        for page in pages:
+            for t in object_types:
+                new_objs = getattr(page, t + "s")
+                if len(new_objs):
+                    objs += new_objs
+                    new_keys = [
+                        k for k, v in new_objs[0].items() if type(v) is not dict
+                    ]
+                    fields = fields.union(set(new_keys))
+
+        serialized = Serializer(precision=precision).serialize(objs)
+
+        cols = CSV_COLS_TO_PREPEND + list(
+            sorted(set(fields) - set(CSV_COLS_TO_PREPEND))
+        )
+
+        w = csv.DictWriter(stream, fieldnames=cols, extrasaction="ignore")
+        w.writeheader()
+        w.writerows(serialized)
+
+        if to_string:
+            stream.seek(0)
+            return stream.read()
+        else:
+            return None

pdfplumber/convert.py
@@ -1,23 +1,9 @@
 import base64
-import csv
-import json
-from io import StringIO
+from typing import Any, Dict, List, Optional, Tuple
+
+from pdfminer.psparser import PSLiteral
 
 from .utils import decode_text
-
-COLS_TO_PREPEND = [
-    "object_type",
-    "page_number",
-    "x0",
-    "x1",
-    "y0",
-    "y1",
-    "doctop",
-    "top",
-    "bottom",
-    "width",
-    "height",
-]
 
 ENCODINGS_TO_TRY = [
     "utf-8",
@@ -27,15 +13,15 @@ ENCODINGS_TO_TRY = [
 ]
 
 
-def to_b64(data_bytes):
-    return base64.b64encode(data_bytes).decode("ascii")
+def to_b64(data: bytes) -> str:
+    return base64.b64encode(data).decode("ascii")
 
 
 class Serializer:
-    def __init__(self, precision=None):
+    def __init__(self, precision: Optional[int] = None):
         self.precision = precision
 
-    def serialize(self, obj):
+    def serialize(self, obj: Any) -> Any:
         if obj is None:
             return None
 
@@ -54,103 +40,34 @@ class Serializer:
         else:
             return str(obj)
 
-    def do_float(self, x):
+    def do_float(self, x: float) -> float:
         return x if self.precision is None else round(x, self.precision)
 
-    def do_bool(self, x):
+    def do_bool(self, x: bool) -> int:
         return int(x)
 
-    def do_list(self, obj):
+    def do_list(self, obj: List[Any]) -> List[Any]:
         return list(self.serialize(x) for x in obj)
 
-    def do_tuple(self, obj):
+    def do_tuple(self, obj: Tuple[Any, ...]) -> Tuple[Any, ...]:
         return tuple(self.serialize(x) for x in obj)
 
-    def do_dict(self, obj):
+    def do_dict(self, obj: Dict[str, Any]) -> Dict[str, Any]:
         return {k: self.serialize(v) for k, v in obj.items()}
 
-    def do_PDFStream(self, obj):
+    def do_PDFStream(self, obj: Any) -> Dict[str, str]:
         return {"rawdata": to_b64(obj.rawdata)}
 
-    def do_PSLiteral(self, obj):
+    def do_PSLiteral(self, obj: PSLiteral) -> str:
         return decode_text(obj.name)
 
-    def do_bytes(self, obj):
+    def do_bytes(self, obj: bytes) -> Optional[str]:
         for e in ENCODINGS_TO_TRY:
             try:
                 return obj.decode(e)
             except UnicodeDecodeError:  # pragma: no cover
-                pass
+                return None
         # If none of the decodings work, raise whatever error
         # decoding with utf-8 causes
         obj.decode(ENCODINGS_TO_TRY[0])  # pragma: no cover
-
-
-def to_json(container, stream=None, types=None, precision=None, indent=None):
-    if types is None:
-        types = list(container.objects.keys()) + ["annot"]
-
-    def page_to_dict(page):
-        d = {
-            "page_number": page.page_number,
-            "initial_doctop": page.initial_doctop,
-            "rotation": page.rotation,
-            "cropbox": page.cropbox,
-            "mediabox": page.mediabox,
-            "bbox": page.bbox,
-            "width": page.width,
-            "height": page.height,
-        }
-        for t in types:
-            d[t + "s"] = getattr(page, t + "s")
-        return d
-
-    if hasattr(container, "pages"):
-        data = {
-            "metadata": container.metadata,
-            "pages": list(map(page_to_dict, container.pages)),
-        }
-    else:
-        data = page_to_dict(container)
-
-    serialized = Serializer(precision=precision).serialize(data)
-
-    if stream is None:
-        return json.dumps(serialized, indent=indent)
-    else:
-        return json.dump(serialized, stream, indent=indent)
-
-
-def to_csv(container, stream=None, types=None, precision=None):
-    if stream is None:
-        stream = StringIO()
-        to_string = True
-    else:
-        to_string = False
-
-    if types is None:
-        types = list(container.objects.keys()) + ["annot"]
-
-    objs = []
-    fields = set()
-
-    pages = container.pages if hasattr(container, "pages") else [container]
-    for page in pages:
-        for t in types:
-            new_objs = getattr(page, t + "s")
-            if len(new_objs):
-                objs += new_objs
-                new_keys = [k for k, v in new_objs[0].items() if type(v) is not dict]
-                fields = fields.union(set(new_keys))
-
-    serialized = Serializer(precision=precision).serialize(objs)
-
-    cols = COLS_TO_PREPEND + list(sorted(set(fields) - set(COLS_TO_PREPEND)))
-
-    w = csv.DictWriter(stream, fieldnames=cols, extrasaction="ignore")
-    w.writeheader()
-    w.writerows(serialized)
-
-    if to_string:
-        stream.seek(0)
-        return stream.read()
+        return None  # pragma: no cover

commit 8a0c12652cfc7e4328be7023b0da60c97308b68d

Restore "text" attr to .textboxhorizontal/etc.
Fix regression introduced in 9587cc7 / v0.6.2.

pdfplumber/page.py
@@ -23,6 +23,7 @@ from pdfminer.layout import (
     LTLine,
     LTPage,
     LTRect,
+    LTTextContainer,
 )
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
@@ -211,8 +212,10 @@ class Page(Container):
         attr["object_type"] = kind
         attr["page_number"] = self.page_number
 
-        if isinstance(obj, LTChar):
+        if isinstance(obj, (LTChar, LTTextContainer)):
             attr["text"] = obj.get_text()
+
+        if isinstance(obj, LTChar):
             gs = obj.graphicstate
             attr["stroking_color"] = gs.scolor
             attr["non_stroking_color"] = gs.ncolor

tests/test_laparams.py
@@ -26,6 +26,8 @@ class Test(unittest.TestCase):
             page = pdf.pages[0]
             assert len(page.textboxhorizontals) == 27
             assert len(page.textlinehorizontals) == 79
+            assert "text" in page.textboxhorizontals[0]
+            assert "text" in page.textlinehorizontals[0]
             assert len(page.chars) == 4408
             assert "anno" not in page.objects.keys()
 
@@ -38,6 +40,8 @@ class Test(unittest.TestCase):
             assert len(page.textboxhorizontals) == 74
             assert len(page.textlineverticals) == 11
             assert len(page.textboxverticals) == 6
+            assert "text" in page.textboxverticals[0]
+            assert "text" in page.textlineverticals[0]
 
     def test_issue_383(self):
         with pdfplumber.open(self.path, laparams={}) as pdf: