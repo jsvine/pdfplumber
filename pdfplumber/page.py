jsvine/pdfplumber

commit 0bfffc2448aca6c9bc8f93109872502cdf12fc6c

Fix Page.get_textmap caching for extra_attrs=[...]
... by preconverting list kwargs to tuples.

pdfplumber/page.py
@@ -167,6 +167,23 @@ class PDFPageAggregatorWithMarkedContent(PDFPageAggregator):
         self.tag_cur_item()
 
 
+def textmap_cacher(func: Callable[..., TextMap]) -> Callable[..., TextMap]:
+    """
+    Caches the kwargs to Page._get_textmap
+    and converts list kwargs (which would be unhashable) into tuples.
+    """
+
+    def new_func(**kwargs: Any) -> TextMap:
+        return lru_cache()(func)(
+            **{
+                key: (tuple(value) if isinstance(value, list) else value)
+                for key, value in kwargs.items()
+            }
+        )
+
+    return new_func
+
+
 class Page(Container):
     cached_properties: List[str] = Container.cached_properties + ["_layout"]
     is_original: bool = True
@@ -211,8 +228,8 @@ class Page(Container):
             )
         )
 
-        # https://rednafi.github.io/reflections/dont-wrap-instance-methods-with-functoolslru_cache-decorator-in-python.html
-        self.get_textmap = lru_cache()(self._get_textmap)
+        # https://rednafi.com/python/lru_cache_on_methods/
+        self.get_textmap = textmap_cacher(self._get_textmap)
 
     @property
     def width(self) -> T_num:
@@ -569,7 +586,7 @@ class DerivedPage(Page):
         self.page_obj = parent_page.page_obj
         self.page_number = parent_page.page_number
         self.flush_cache(Container.cached_properties)
-        self.get_textmap = lru_cache()(self._get_textmap)
+        self.get_textmap = textmap_cacher(self._get_textmap)
 
 
 def test_proposed_bbox(bbox: T_bbox, parent_bbox: T_bbox) -> None:

tests/test_utils.py
@@ -88,6 +88,27 @@ class Test(unittest.TestCase):
         assert words_rtl[1]["text"] == "baaabaaA/AAA"
         assert words_rtl[1]["direction"] == -1
 
+    def test_extra_attrs(self):
+        path = os.path.join(HERE, "pdfs/extra-attrs-example.pdf")
+        with pdfplumber.open(path) as pdf:
+            page = pdf.pages[0]
+            assert page.extract_text() == "BlackRedArial"
+            assert (
+                page.extract_text(extra_attrs=["non_stroking_color"])
+                == "Black RedArial"
+            )
+            assert page.extract_text(extra_attrs=["fontname"]) == "BlackRed Arial"
+            assert (
+                page.extract_text(extra_attrs=["non_stroking_color", "fontname"])
+                == "Black Red Arial"
+            )
+            # Should not error
+            assert page.extract_text(
+                layout=True,
+                use_text_flow=True,
+                extra_attrs=["non_stroking_color", "fontname"],
+            )
+
     def test_extract_words_punctuation(self):
         path = os.path.join(HERE, "pdfs/test-punkt.pdf")
         with pdfplumber.open(path) as pdf:

commit efca2770795f406469c80ede9924145ff0704719

Fix layout-caching issue (#1097) caused by 0bfffc2
Layout wasn't actually getting cached, now it is.

pdfplumber/page.py
@@ -118,6 +118,13 @@ def normalize_color(
     return separate_pattern(tuplefied)
 
 
+def tuplify_list_kwargs(kwargs: Dict[str, Any]) -> Dict[str, Any]:
+    return {
+        key: (tuple(value) if isinstance(value, list) else value)
+        for key, value in kwargs.items()
+    }
+
+
 class PDFPageAggregatorWithMarkedContent(PDFPageAggregator):
     """Extract layout from a specific page, adding marked-content IDs to
     objects where found."""
@@ -167,23 +174,6 @@ class PDFPageAggregatorWithMarkedContent(PDFPageAggregator):
         """Hook for rendering lines and curves, adding the `mcid` attribute."""
         super().paint_path(*args, **kwargs)
         self.tag_cur_item()
-
-
-def textmap_cacher(func: Callable[..., TextMap]) -> Callable[..., TextMap]:
-    """
-    Caches the kwargs to Page._get_textmap
-    and converts list kwargs (which would be unhashable) into tuples.
-    """
-
-    def new_func(**kwargs: Any) -> TextMap:
-        return lru_cache()(func)(
-            **{
-                key: (tuple(value) if isinstance(value, list) else value)
-                for key, value in kwargs.items()
-            }
-        )
-
-    return new_func
 
 
 def _normalize_box(box_raw: T_bbox, rotation: T_num = 0) -> T_bbox:
@@ -251,7 +241,7 @@ class Page(Container):
         self.bbox = self.mediabox
 
         # See https://rednafi.com/python/lru_cache_on_methods/
-        self.get_textmap = textmap_cacher(self._get_textmap)
+        self.get_textmap = lru_cache()(self._get_textmap)
 
     def close(self) -> None:
         self.flush_cache()
@@ -493,7 +483,7 @@ class Page(Container):
         return_groups: bool = True,
         **kwargs: Any,
     ) -> List[Dict[str, Any]]:
-        textmap = self.get_textmap(**kwargs)
+        textmap = self.get_textmap(**tuplify_list_kwargs(kwargs))
         return textmap.search(
             pattern,
             regex=regex,
@@ -504,7 +494,7 @@ class Page(Container):
         )
 
     def extract_text(self, **kwargs: Any) -> str:
-        return self.get_textmap(**kwargs).as_string
+        return self.get_textmap(**tuplify_list_kwargs(kwargs)).as_string
 
     def extract_text_simple(self, **kwargs: Any) -> str:
         return utils.extract_text_simple(self.chars, **kwargs)
@@ -515,7 +505,7 @@ class Page(Container):
     def extract_text_lines(
         self, strip: bool = True, return_chars: bool = True, **kwargs: Any
     ) -> T_obj_list:
-        return self.get_textmap(**kwargs).extract_text_lines(
+        return self.get_textmap(**tuplify_list_kwargs(kwargs)).extract_text_lines(
             strip=strip, return_chars=return_chars
         )
 
@@ -625,7 +615,7 @@ class DerivedPage(Page):
         self.mediabox = parent_page.mediabox
         self.cropbox = parent_page.cropbox
         self.flush_cache(Container.cached_properties)
-        self.get_textmap = textmap_cacher(self._get_textmap)
+        self.get_textmap = lru_cache()(self._get_textmap)
 
 
 def test_proposed_bbox(bbox: T_bbox, parent_bbox: T_bbox) -> None: