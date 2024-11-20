4de84f64e7c6fac492063ee38cc612cb8f0c3a63
David Huggins-Daines <dhd@ecolingui.ca>
2024-11-20 00:00:51 -0500

    fix(tests): PLAYA fixed its colors

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -178,7 +178,7 @@ class Test(unittest.TestCase):
         assert c.split("\r\n")[9] == (
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             '18.0,12.996,,,,,,,TimesNewRomanPSMT,,,"(1, 0, 0, 1, 45.83, 660.69)"'
-            ',,,DeviceRGB,"(0.0, 0.0, 0.0)",,,,DeviceGray,18.0,,,,0,,,Y,,1,'
+            ',,,DeviceRGB,"(0, 0, 0)",,,,DeviceGray,18.0,,,,0,,,Y,,1,'
         )
 
         io = StringIO()
@@ -253,7 +253,7 @@ class Test(unittest.TestCase):
         assert lines[9] == (
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             '18.0,12.996,,,,,,,TimesNewRomanPSMT,,,"(1, 0, 0, 1, 45.83, 660.69)"'
-            ',,,DeviceRGB,"(0.0, 0.0, 0.0)",,,,DeviceGray,18.0,,,,0,,,Y,,1,'
+            ',,,DeviceRGB,"(0, 0, 0)",,,,DeviceGray,18.0,,,,0,,,Y,,1,'
         )
 
     def test_cli_csv_exclude(self):
@@ -287,7 +287,7 @@ class Test(unittest.TestCase):
         assert lines[9] == (
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             "18.0,12.996,,,,,,,TimesNewRomanPSMT,,"
-            ',,"(0.0, 0.0, 0.0)",,,DeviceGray,18.0,,,,0,,Y,,1,'
+            ',,"(0, 0, 0)",,,DeviceGray,18.0,,,,0,,Y,,1,'
         )
 
     def test_cli_csv_include(self):
