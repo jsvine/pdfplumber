56bcab6c447f0a5a18c601453bb8d1894f14b094
David Huggins-Daines <dhd@ecolingui.ca>
2024-12-13 09:05:18 -0500

    feat: lightly wrap playa structure

--- pdfplumber/structure.py
+++ pdfplumber/structure.py
@@ -1,8 +1,39 @@
 from collections import deque
-from dataclasses import asdict
-from typing import Any, Dict
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, Iterator, Optional, TYPE_CHECKING
 
-from playa.structtree import StructElement
+from playa.structtree import StructElement, StructTree
+
+if TYPE_CHECKING:  # pragma: nocover
+    from .pdf import PDF
+    from .page import Page
+
+
+class StructTreeMissing(ValueError):
+    pass
+
+
+class PDFStructElement(StructElement):
+    """PDF Logical Structure Element"""
+
+    # When PLAYA changes its API, wrap it here...
+
+
+class PDFStructTree(StructTree):
+    """PDF Logical Structure Tree"""
+
+    def __init__(self, doc: "PDF", page: Optional["Page"] = None):
+        self.doc = doc.doc
+        try:
+            if page is not None:
+                pages = [page.page_obj]
+            else:
+                pages = None
+            super().__init__(self.doc, pages)
+        except KeyError:
+            raise StructTreeMissing("PDF has no structure")
+
+    # May wish to wrap __iter__, find, find_all in the future
 
 
 def structure_dict(top: StructElement) -> Dict[str, Any]:
