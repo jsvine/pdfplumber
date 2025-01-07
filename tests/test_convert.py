c9d3848d30a5d5e1c5646b75648f880b9f47a281
David Huggins-Daines <dhd@ecolingui.ca>
2025-01-07 10:36:18 -0500

    fix(tests): new and more correct text objects

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -179,7 +179,7 @@ class Test(unittest.TestCase):
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             "18.0,12.996,,,,,,,TimesNewRomanPSMT,,0,"
             '"(1.0, 0.0, 0.0, 1.0, 45.83, 660.69)"'
-            ',,,DeviceGray,"(0,)",,,,0,DeviceGray,18.0,,,,"(0,)",,,Y,,1,'
+            ',,,DeviceRGB,"(0, 0, 0)",,,,0,DeviceGray,18.0,,,,"(0,)",,,Y,,1,'
         )
 
         io = StringIO()
@@ -256,7 +256,7 @@ class Test(unittest.TestCase):
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             "18.0,12.996,,,,,,,TimesNewRomanPSMT,,0,"
             '"(1.0, 0.0, 0.0, 1.0, 45.83, 660.69)"'
-            ',,,DeviceGray,"(0,)",,,,0,DeviceGray,18.0,,,,"(0,)",,,Y,,1,'
+            ',,,DeviceRGB,"(0, 0, 0)",,,,0,DeviceGray,18.0,,,,"(0,)",,,Y,,1,'
         )
 
     def test_cli_csv_exclude(self):
@@ -290,7 +290,7 @@ class Test(unittest.TestCase):
         assert lines[9] == (
             "char,1,45.83,58.826,656.82,674.82,117.18,117.18,135.18,12.996,"
             "18.0,12.996,,,,,,,TimesNewRomanPSMT,,"
-            '0,,"(0,)",,,0,DeviceGray,18.0,,,,"(0,)",,Y,,1,'
+            '0,,"(0, 0, 0)",,,0,DeviceGray,18.0,,,,"(0,)",,Y,,1,'
         )
 
     def test_cli_csv_include(self):
