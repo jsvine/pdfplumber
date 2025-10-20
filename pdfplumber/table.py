7ae4e259f05e1258664794b4c9ce0f5f4720a459
Yonah Tal-Meyer <50260601+DoomGuy77@users.noreply.github.com>
2025-10-20 18:52:30 +1100

    actually fix the attribute errors properly

--- pdfplumber/table.py
+++ pdfplumber/table.py
@@ -304,10 +304,10 @@ def find_edge_cells(
 
     def point_intersects_edge(x: int, y: int, edge: T_obj) -> bool:
         if edge["orientation"] == 'h':
-            if (abs(y - edge[3]) <= y_tolerance) and (min(edge[2], edge.x2) - x_tolerance <= x <= max(edge[2], edge.x2) + x_tolerance):
+            if (abs(y - edge["y1"]) <= y_tolerance) and (min(edge["x1"], edge.x2) - x_tolerance <= x <= max(edge["x1"], edge.x2) + x_tolerance):
                 return True
         elif edge["orientation"] == 'v':
-            if (abs(x - edge[2]) <= x_tolerance) and (min(edge[3], edge.y2) - y_tolerance <= y <= max(edge[3], edge.y2) + y_tolerance):
+            if (abs(x - edge["x1"]) <= x_tolerance) and (min(edge["y1"], edge.y2) - y_tolerance <= y <= max(edge["y1"], edge.y2) + y_tolerance):
                 return True
         return False
     
@@ -343,19 +343,19 @@ def find_edge_cells(
     for edge in edges:
         ints = cell_intersections(edge)
         if edge["orientation"] == 'h':
-            if edge[0] < ints[0] - x_tolerance:
-                if not already_made_cell(edge[0], edge[1]):
-                    new_cells.append((edge[0], edge[1], ints[0][0], ints[0][3]))
-            if edge[2] > ints[1][2] + x_tolerance:
-                if not already_made_cell(ints[1][2], edge[1]):
-                    new_cells.append((ints[1][2], edge[1], edge[2], ints[1][3]))
+            if edge["x0"] < ints[0] - x_tolerance:
+                if not already_made_cell(edge["x0"], edge["y0"]):
+                    new_cells.append((edge["x0"], edge["y0"], ints[0][0], ints[0][3]))
+            if edge["x1"] > ints[1][2] + x_tolerance:
+                if not already_made_cell(ints[1][2], edge["y0"]):
+                    new_cells.append((ints[1][2], edge["y0"], edge["x1"], ints[1][3]))
         if edge["orientation"] == 'v':
-            if edge[1] < ints[0][1] - y_tolerance:
-                if not already_made_cell(edge[0], edge[1]):
-                    new_cells.append((edge[0], edge[1], ints[0][0], ints[0][3]))
-            if edge[3] > ints[1][3] + y_tolerance:
-                if not already_made_cell(ints[1][2], edge[1]):
-                    new_cells.append((ints[1][2], edge[1], edge[2], ints[1][3]))
+            if edge["y0"] < ints[0][1] - y_tolerance:
+                if not already_made_cell(edge["x0"], edge["y0"]):
+                    new_cells.append((edge["x0"], edge["y0"], ints[0][0], ints[0][3]))
+            if edge["y1"] > ints[1][3] + y_tolerance:
+                if not already_made_cell(ints[1][2], edge["y0"]):
+                    new_cells.append((ints[1][2], edge["y0"], edge["x1"], ints[1][3]))
     return new_cells
 
             
