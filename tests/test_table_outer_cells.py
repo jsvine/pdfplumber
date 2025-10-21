a36b4535d996cc997697cceca9aa3b3c3d638e0a
Bradley Lunn <u7923257@anu.edu.au>
2025-10-21 23:48:17 +1100

    Basic version to check things are working. REQUIRES WORK

--- tests/test_table_outer_cells.py
+++ tests/test_table_outer_cells.py
@@ -2,12 +2,14 @@ import os
 import unittest
 import pytest
 import pdfplumber
+import json
+
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 class Test(unittest.TestCase):
     @classmethod
     def setup_class(self):
-        path = os.path.join(HERE, "pdfs/pdffill-demo.pdf")
+        path = os.path.join(HERE, "pdfs/issue-1325.pdf")
         self.pdf = pdfplumber.open(path)
 
     @classmethod
@@ -15,9 +17,28 @@ class Test(unittest.TestCase):
         self.pdf.close()
         
     def test_lacking_outer_borders(self):
-            # See https://github.com/jsvine/pdfplumber/issues/1325    
-            
+            # See https://github.com/jsvine/pdfplumber/issues/1325             
         path = os.path.join(HERE, "pdfs/issue-1325.pdf")
         with pdfplumber.open(path) as pdf:
-            pdf.pages[1].to_image(150).debug_tablefinder(table_settings={}).show()
+            #pdf.pages[1].to_image(300).debug_tablefinder(table_settings={}).show()
+            p1 = pdf.pages[1]
+            p1 = p1.within_bbox((30, 295, p1.width-30, p1.height-60)) 
+            #p1.to_image(600).debug_tablefinder(table_settings={}).show()
+            # bbox is (posXstart posYstart posXend posYend)
+            
+            extract = p1.extract_table(table_settings={})
+                
+            with open("output.json", "w") as F: # TODO make it not output!
+                json.dump(extract, F, indent=4)
+                
+                if extract[1] == ["kognitive\nLernvoraussetzungen","motivationale\nLernvoraussetzungen","emotionale\nLernvoraussetzungen", "inhaltliche\Bedingungen"]:
+                    F.write("\nHeaders DO work")
+                else:
+                    F.write("\nHeaders DO NOT work")
+                    
+            assert extract[1] == ["kognitive\nLernvoraussetzungen","motivationale\nLernvoraussetzungen","emotionale\nLernvoraussetzungen", "inhaltliche\Bedingungen"]
+            
+            #F.write(p1.extract_text())
+            #p1.extract_table(table_settings={})          
+            
         
