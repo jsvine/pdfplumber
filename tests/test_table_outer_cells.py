763a8486ea3b4842344b12e9df83af657631cfa8
Bradley Lunn <u7923257@anu.edu.au>
2025-10-21 23:02:14 +1100

    Adjusted the code stub to run with pytest.

--- tests/test_table_outer_cells.py
+++ tests/test_table_outer_cells.py
@@ -1,4 +1,23 @@
+import os
+import unittest
+import pytest
 import pdfplumber
+HERE = os.path.abspath(os.path.dirname(__file__))
 
-with pdfplumber.open("./pdfs/issue-1325.pdf") as pdf:
-    pdf.pages[1].to_image(150).debug_tablefinder(table_settings={}).show()
+class Test(unittest.TestCase):
+    @classmethod
+    def setup_class(self):
+        path = os.path.join(HERE, "pdfs/pdffill-demo.pdf")
+        self.pdf = pdfplumber.open(path)
+
+    @classmethod
+    def teardown_class(self):
+        self.pdf.close()
+        
+    def test_lacking_outer_borders(self):
+            # See https://github.com/jsvine/pdfplumber/issues/1325    
+            
+        path = os.path.join(HERE, "pdfs/issue-1325.pdf")
+        with pdfplumber.open(path) as pdf:
+            pdf.pages[1].to_image(150).debug_tablefinder(table_settings={}).show()
+        
