ebafd6e9cb81afcfee0e4d79d119d3058490467a
Jeremy Singer-Vine <jsvine@gmail.com>
2026-01-28 23:22:30 +0100

    Update Python versions in setup.py classifiers

--- setup.py
+++ setup.py
@@ -50,9 +50,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
 )
